96565e6762f8b65d79ca8b0800a6298fdcdcf399
unknown <D:\The Bat!>
2021-01-28 11:04:39 +0300

    task 2 completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 // task 1
 console.log('Задание 1. Создать переменные, получить данные через prompt, вывести на экран фразу');
 
-let name = prompt('Ваше имя?'),
+let firstName = prompt('Ваше имя?'),
     age = prompt('Ваш вораст?'), 
     city = prompt('Ваш город проживания?'), 
     phone = prompt('Ваш телефон?'), 
     email = prompt('Ваша электронная почта?'), 
     company = prompt('Ваше место работы?'); 
 
-console.log('Меня зовут ' + name + '. Мне ' + age + ' лет. Я проживаю в городе ' + city + ' и работаю в компании ' + company + '. Мои контактные данные: ' + phone + ', ' + email + '.');
+console.log('Меня зовут ' + firstName + '. Мне ' + age + ' лет. Я проживаю в городе ' + city + ' и работаю в компании ' + company + '. Мои контактные данные: ' + phone + ', ' + email + '.');
+
+// task 2
+console.log('Задание 2. Определить по введенному возрасту год рождения');
+
+let year = 2021 - age;
+
+console.log(firstName + ' родился в ' + year + ' году.');
